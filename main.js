bbf906fe407ffd724f17657c3c69a910f3e58c29
Yaser.ta <yasertarrab10@gmail.com>
2024-11-02 20:46:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+console.log("welcom to js task")
+
+
+// 1 حل السوال الاول من التاسك//
+let num1 = 40;
+let num2 = 4;
+let sum = num1 + num2;
+let eksi= num1 - num2;
+let bolme=num1 / num2;
+let carpma=num1 * num2;
+let kalan=num1 % num2;
+console.log("Toplam mi:",sum)
+console.log("Cikari mi:",eksi)
+console.log("Bolumu:",bolme)
+console.log("Carpimi:",carpma)
+console.log("Bolumun kalani:",kalan)
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+// 2 حل السوال الاول من التاسك//
+
+let buyuktur= num2<num1;
+let kucuuktur= num1>num2;
+let esittir = num1 == num2;
+let esit_degil= num1 !== num2
+
+console.log("num1 buyuktur num2",buyuktur)
+console.log("num2 kucuktur num1",kucuuktur)
+console.log("num1 esittir num2",esittir)
+console.log("num1 esit degil num2",esit_degil)
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+
+let isim= "Yaser";
+let soyad="Tarrab";
+let full_name=isim + " " + soyad;
+console.log("AD SOYAD:",full_name);
